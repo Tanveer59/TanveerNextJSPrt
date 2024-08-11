19a36e229e55e7c6835b24bb4f8c802d3014c825
Tanveer59 <tanveer1234awan@gmail.com>
2024-08-12 00:15:21 +0500

    justigy

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -3,7 +3,7 @@ import { Link } from "theme-ui";
 
 const Product = (props) => {
     return (
-        <div className="border p-2 rounded-md relative shadow-sm fadeOut min-h-[500px]" >
+        <div className="border flex flex-col justify-between p-2 rounded-md relative shadow-sm fadeOut min-h-[500px]" >
             <div className="absolute bg-white p-2 rounded-sm">
                 <div className="flex justify-start items-center gap-2">
                     <div className={`${props.clr} w-4 h-4 rounded-full`}></div>
