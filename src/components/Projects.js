6111db78962a4fdc47c80c9e2b327c0e3eb98c74
Tanveer59 <tanveer1234awan@gmail.com>
2024-08-12 00:04:34 +0500

    im

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -34,9 +34,9 @@ const Projects = () => {
 
             <div className="flex flex-col w-full mt-10 gap-10">
                 {displayedProjects.map((projectSet, setIndex) => (
-                        <div key={setIndex} className="flex flex-col md:flex-row w-full justify-between gap-2 md:flex-wrap lg:flex-nowrap min-h-[500px]">
+                        <div key={setIndex} className="flex flex-col md:flex-row w-full justify-between gap-2 md:flex-wrap lg:flex-nowrap ">
                             {projectSet.map((item, index) => (
-                                <div key={`${setIndex}-${index}`} className="w-full md:w-1/2 lg:w-1/4">
+                                <div key={`${setIndex}-${index}`} className="w-full md:w-1/2 lg:w-1/4 min-h-[500px]">
                                     <Product src={item.src} tool={item.tool} link={item.link} st={item.state} clr={item.color}/>
                                 </div>
                             ))}
